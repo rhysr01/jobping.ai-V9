38eb082a05b2912112a81259257030edcb3d9d90
rhys rowlands <rhysrowlands@Mac.station>
2025-12-30 13:30:13 +0000

    fix: add re-export files for scrapers/types and scrapers/utils
    - Create scrapers/types.ts that re-exports from _tools_archive
    - Create scrapers/utils.ts that re-exports from _tools_archive
    - Fixes build error: Cannot find module '../../scrapers/types'
    - Maintains backwards compatibility for existing imports

---
+++ scrapers/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * Scraper Utils
+ * Re-exported from _tools_archive for backwards compatibility
+ * 
+ * @deprecated This file is kept for backwards compatibility.
+ * The utils are archived in _tools_archive/scrapers/utils.ts
+ */
+
+export * from "../_tools_archive/scrapers/utils";
